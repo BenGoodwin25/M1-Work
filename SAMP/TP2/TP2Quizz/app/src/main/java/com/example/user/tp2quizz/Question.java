7b006d2669f94619536a3db7c4ba38a03989d436
Benjamin Goodwin <goodb25@gmail.com>
2017-11-12 12:56:15 +0100

    Finished Play fonctionality

--- SAMP/TP2/TP2Quizz/app/src/main/java/com/example/user/tp2quizz/Question.java
+++ SAMP/TP2/TP2Quizz/app/src/main/java/com/example/user/tp2quizz/Question.java
@@ -1,5 +1,6 @@
 package com.example.user.tp2quizz;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -17,6 +18,11 @@ public class Question extends AppCompatActivity {
     SQLiteDatabase db;
     DatabaseHelper DBhelper;
 
+    Cursor quiz;
+    Cursor nbProposition;
+
+    int score;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,50 +35,69 @@ public class Question extends AppCompatActivity {
         Bundle b = getIntent().getExtras();
         int quizzId = (b.getInt("id") + 1);
         System.out.println(quizzId);
-        final Cursor quiz = db.rawQuery("SELECT * FROM Question WHERE quizz = " + quizzId, null, null);
+        quiz = db.rawQuery("SELECT * FROM Question WHERE quizz = " + quizzId, null,
+                null);
 
         if(!quiz.moveToFirst()){
             Log.i("DATABASE","Init BDD");
         } else {
-            String[] tmpArray= new String[10];
+            score=0;
+            question();
+        }
+    }
+
+    public void question(){
+        if(quiz.isAfterLast()){
+            Intent i = new Intent(Question.this, ScoreActivity.class);
+            i.putExtra("score", score);
+            startActivity(i);
+        }
+        else {
+
+            String[] tmpArray = new String[10];//MAX Propositions possible
+            int[] idArray = new int[10];
             int index = 0;
-            int answer = quiz.getInt(2);
-            tmpArray[index] = quiz.getString(1);
-            System.out.println(quiz.getString(2));
-            index++;
-            final Cursor nbProposition = db.rawQuery("SELECT * FROM Proposition WHERE question =" + quiz.getShort(0), null, null);
-            quiz.moveToNext();
+            TextView TView=(TextView)findViewById(R.id.textView2);
+            TView.setText("" + quiz.getString(1));
+
+            nbProposition = db.rawQuery("SELECT * FROM Proposition WHERE question =" +
+                    quiz.getShort(0), null, null);
             nbProposition.moveToFirst();
+            final int answer=quiz.getInt(2)-nbProposition.getInt(0);
+            System.out.println("answer : " + answer);
             while (!nbProposition.isAfterLast()) {
+                idArray[index] = nbProposition.getInt(0);
                 tmpArray[index] = nbProposition.getString(1);
                 System.out.println(nbProposition.getString(1));
                 index++;
                 nbProposition.moveToNext();
             }
             final String[] mobileArray = new String[index];
-            for(int i=0; i<index;i++){
-                mobileArray[i]=tmpArray[i];
-            }
-            ArrayAdapter adapter = new ArrayAdapter<String>(this,
-                    R.layout.questions_items, mobileArray);
-
-
             final ListView listView = (ListView) findViewById(R.id.question_list);
+            for (int i = 0; i < index; i++) {
+                mobileArray[i] = tmpArray[i];
+            }
+            ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.questions_items,
+                    mobileArray);
 
             listView.setAdapter(adapter);
-
             listView.setClickable(true);
+
             listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                     // When clicked, show a toast with the TextView text or do whatever you need.
-                    if(position+1==answer){
-                        Toast.makeText(getApplicationContext(), "True", Toast.LENGTH_SHORT).show();
+                    if(position==answer){
+                        score++;
+                        quiz.moveToNext();
+                        question();
                     } else {
-                        Toast.makeText(getApplicationContext(), "False", Toast.LENGTH_SHORT).show();
+                        quiz.moveToNext();
+                        question();
                     }
 
                 }
             });
+
         }
     }
 
